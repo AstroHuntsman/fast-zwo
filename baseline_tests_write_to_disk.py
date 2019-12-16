09f303468ec2ae98158beba0af488d99199d4df9
Huntsman <huntsman.array@gmail.com>
2019-12-16 11:29:11 +1100

    lee test in office of fps

--- baseline_tests_write_to_disk.py
+++ baseline_tests_write_to_disk.py
@@ -1,16 +1,17 @@
 import time
-import numpy as np
-# from numpy_ringbuffer import RingBuffer
+
 from asi import ASICamera
 
 from astropy.io import fits
 
+import numpy as np
+
 def frame_rate_test(camera, n=100):
     start_time = time.monotonic()
     camera.start_video_capture()
-    #image_array = RingBuffer(capacity=10, dtype=(np.uint16, 3672, 5496))
-    image_array = []
+
     image_data_list = []
+
     for i in range(n):
         image = camera.get_video_data()
 
@@ -22,15 +23,8 @@ def frame_rate_test(camera, n=100):
             hdu.writeto(f'new_{i}.fits', overwrite=True)
             del hdu
             image_data_list = []
-
-        #Start Brint's code
-
-        #if image is not None:
-        #    #do stuff
-        #    image_array.append(image)
-        #    diff = image_array[0] - image
-
-
+        # print(image)
+        # break
     end_time = time.monotonic()
     camera.stop_video_capture()
     fps = n / (end_time - start_time)
@@ -41,4 +35,5 @@ def frame_rate_test(camera, n=100):
 
 if __name__ == '__main__':
     camera = ASICamera(library_path='/Applications/ASICAP.app/Contents/MacOS/libASICamera2.dylib')
-    frame_rate_test(camera, n=1000)
+    print("Do nothing test:")
+    frame_rate_test(camera, n=500)
